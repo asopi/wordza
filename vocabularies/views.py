5db1cb935510760028214f6f579b1bebcff1062b
Alfred Sopi <alfred.sopi@hotmail.com>
2020-11-26 20:59:33 +0100

    integration of google cloud translate api and first implemented first translation calls

--- vocabularies/views.py
+++ vocabularies/views.py
@@ -6,6 +6,10 @@ from .models import Vocable
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.forms import inlineformset_factory
+from google.cloud import translate_v2 as translate
+from django.http import HttpResponse
+
+translate_client = translate.Client()
 
 
 @login_required
@@ -21,8 +25,6 @@ def vocabulary_view(request, *args, **kwargs):
 
 @login_required
 def create_view(request, *args, **kwargs):
-    print('test')
-    print(request)
     if request.method == 'POST':
         voc_form = VocabulariesForm(request.POST or None)
 
@@ -39,11 +41,25 @@ def create_view(request, *args, **kwargs):
 
 
 @login_required
+def translate_vocable(request):
+    if request.method == 'POST':
+        value = request.POST["value"]
+        language = request.POST["language"]
+        targed_language = request.POST["target_language"]
+        response = translate_client.translate(
+            value, source_language=language, target_language=targed_language)
+        return HttpResponse(response["translatedText"])
+    else:
+        return HttpResponse("")
+
+
+@login_required
 def edit_view(request, vocabulary_id):
     vocabulary = get_object_or_404(Vocabulary, pk=vocabulary_id)
     VocabularyFormSet = inlineformset_factory(
         Vocabulary, Vocable, fields=('value', 'translated_value'), extra=1)
     form = VocabulariesForm(instance=vocabulary)
+
     form_set = VocabularyFormSet(
         queryset=Vocable.objects.none(), instance=vocabulary)
     vocables = Vocable.objects.filter(vocabulary_id=vocabulary_id)
@@ -51,9 +67,12 @@ def edit_view(request, vocabulary_id):
     if request.method == 'POST':
         form = VocabulariesForm(request.POST or None, instance=vocabulary)
         form_set = VocabularyFormSet(request.POST or None, instance=vocabulary)
+
         if form_set.is_valid() and form.is_valid():
+
             form.save()
             form_set.save()
+
             vocabulary = get_object_or_404(Vocabulary, pk=vocabulary_id)
             VocabularyFormSet = inlineformset_factory(
                 Vocabulary, Vocable, fields=('value', 'translated_value'), extra=1)
@@ -64,6 +83,7 @@ def edit_view(request, vocabulary_id):
                 "form": form,
                 "form_set": form_set,
                 "vocables": vocables,
+                "vocabulary": vocabulary,
                 "message": "Successful!",
             }
             return render(request, "vocabularies/edit_view.html", context)
@@ -72,5 +92,6 @@ def edit_view(request, vocabulary_id):
         "form": form,
         "form_set": form_set,
         "vocables": vocables,
+        "vocabulary": vocabulary,
     }
     return render(request, "vocabularies/edit_view.html", context)
