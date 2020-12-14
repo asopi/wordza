45644f68955a85f834745931c60b882fce3ab10b
Alfred Sopi <alfred.sopi@hotmail.com>
2020-12-14 18:18:50 +0100

    refactoring on dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from cards.models import LearnSet
+from vocabularies.models import Vocabulary, Vocable
 
 
 @login_required
 def dashboard_view(request):
-    learn_sets = LearnSet.objects.order_by('-creation_date')
+    learn_sets = LearnSet.objects.filter(user=request.user).order_by('-creation_date')
+    vocabularies = Vocabulary.objects.filter(user=request.user).order_by('-creation_date')
+    vocables = Vocable.objects.filter(user=request.user).order_by('-creation_date')
 
     context = {
-        'title': 'DASHBOARD',
         'learn_sets': learn_sets,
+        'vocabularies': vocabularies,
+        'vocables': vocables,
     }
 
     return render(request, 'dashboard/dashboard.html', context)
